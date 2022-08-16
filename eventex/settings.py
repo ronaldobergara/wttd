072b143dd3b0dea57b19f1bfc55c85caa6b5f8be
ronaldobergara <bergaraheadbanger@gmail.com>
2022-08-16 18:31:54 -0300

    Teste

--- eventex/settings.py
+++ eventex/settings.py
@@ -123,3 +123,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+print(BASE_DIR)
